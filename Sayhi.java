082aa1a5909c70f6c72c01d6a2f2ac3329a4c2ee
JT Leong <1433408558@qq.com>
2019-09-09 21:10:49 +0800

    Sayhi test

---
+++ Sayhi.java
@@ -0,0 +1,10 @@
+public class Sayhi{
+    public static void main(String[] args) {
+	    System.out.println("Below is the function of hi");
+		sayhi();
+	}
+
+	public static void sayhi(){
+	    System.out.println("say Hi");
+	}
+}
